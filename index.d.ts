bd6a4cb0880fbf53209b9a6a72449591e519ffbb
Ion Gireada <shytiger@yahoo.com>
2023-06-13 23:22:08 +0300

    Changes to .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,23 @@
 declare module "hbsr";
+
+
+/**
+ * Render template file with values.
+ * 
+ * @param {string} basename - Template file basename (no extension).
+ * @param {object} data - the values to pass into template.
+ * @param {TemplateSetting} [options={}] - the settings for the template.
+ * 
+ * @returns string by filling template file.
+ */
+export function render_template(basename: string, data: any, options: any) : string;
+
+/**
+ * Renders a template with the given data.
+ * @param {string} source - the source of the template
+ * @param {object} [data={}] - the data to pass to the template
+ * @param {TemplateSetting} [options={}] - the options to pass to the template
+ * @returns {string} the rendered template
+ */
+export function render(source: string, data: any, options: any): string;
+export const options: {template_path: string; template_extension: string;}
